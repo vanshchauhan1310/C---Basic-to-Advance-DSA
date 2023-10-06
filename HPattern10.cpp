d36ee966084c8192476cf464eaf1a2044596871e
Vansh Raj Chauhan <Vanshchauhan1310@gmail.com>
2023-10-06 23:44:23 +0530

    Hard Pattern Added

---
+++ HPattern10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int row,col;
+int n;
+
+cout<<"Enter the Number of Row ";
+cin>>n;
+
+for(row=1;row<=n;row++){
+
+for(col=1;col<=n-row;col++){
+cout<<" ";
+}
+
+for(col=1;col<=2*row-1;col++){
+cout<<"*";
+}
+cout<<endl;
+
+
+}
+
+}
---
+++ Hpattern11.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+int row,col;
+
+cout<<"Enter The Number Of Row ";
+cin>>n;
+
+for(row=1;row<=n;row++){
+
+for(col=1;col<=n-row;col++){
+cout<<" ";
+}
+
+for(col=1;col<=row;col++){
+cout<<col;
+}
+
+for(col=row-1;col>=1;col--){
+cout<<col;
+}
+cout<<endl;
+}
+
+}
+
